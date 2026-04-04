f638f89bafaaa2e2de7246870193809ce19215ba
Amritpreet Singh <amritpreet7889@gmail.com>
2026-04-04 15:51:05 +0530

    Delete blog website directory

--- blog website/public/homepage-script.js
+++
@@ -1,8 +0,0 @@
-$(".view-btn").on("click", function () {
-  const id = $(this).data("id");
-  window.location.href = "/blog/" + id;
-});
-
-$(".add-card").on("click", function () {
-  window.location.href = "/add-blog/" ;
-});
